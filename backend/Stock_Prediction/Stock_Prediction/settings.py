e1b5de8da315c3ee9dc0a2a19cc752c74ab74e7b
Venu Kumar <rishikumar54020@gmail.com>
2025-11-20 15:11:16 +0530

    Fourth Commit

--- backend/Stock_Prediction/Stock_Prediction/settings.py
+++ backend/Stock_Prediction/Stock_Prediction/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +54,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS =[
+    'http://localhost:5173'
+]
 ROOT_URLCONF = 'Stock_Prediction.urls'
 
 TEMPLATES = [
