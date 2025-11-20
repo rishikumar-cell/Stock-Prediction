f5588b3e86a7a2b9dc593edfcc193d1225e42b32
Venu Kumar <rishikumar54020@gmail.com>
2025-11-20 12:24:14 +0530

    third commit

---
+++ backend/Stock_Prediction/accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views import generic
+from .serializer import UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+# Create your views here.
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    
+
